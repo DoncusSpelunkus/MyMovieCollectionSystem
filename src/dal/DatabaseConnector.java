8327d86a5eee10a91a4581b049b4507ab89e33a9
MadsMGrin <business.madsmadsen@gmail.com>
2022-01-04 13:05:35 +0100

    Database Commit

---
+++ src/dal/DatabaseConnector.java
@@ -0,0 +1,23 @@
+package dal;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
+public class DatabaseConnector {
+    private SQLServerDataSource dataSource;
+
+    public DatabaseConnector() {
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName("MovieCollectionSystem");
+        dataSource.setUser("CSe21B_16");
+        dataSource.setPassword("CSe21B_16");
+        dataSource.setPortNumber(1433);
+        dataSource.setServerName("10.176.111.31");
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+}
