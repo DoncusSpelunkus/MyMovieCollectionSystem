37c813318ed6e332cf5fe2b4f3ce2efdaed758fb
tunayerkus <tuna0043@easv365.dk>
2022-01-19 10:02:03 +0100

    .

--- src/gui/Controller/MainController.java
+++ src/gui/Controller/MainController.java
@@ -9,25 +9,21 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
-
-import javafx.scene.control.*;
-
-import java.io.File;
 
 import java.io.IOException;
 import java.net.URL;
@@ -69,7 +65,12 @@ public class MainController implements Initializable {
         @FXML
         private TextField searchField;
 
+        @FXML
+        private MediaView mediaView;
+
         private MediaPlayer mediaPlayer;
+
+
 
 
 
@@ -87,6 +88,8 @@ public class MainController implements Initializable {
     }
 
     public MainController() {
+        this.mediaView = mediaView;
+        this.mediaPlayer = mediaPlayer;
         moviesModel = new MoviesModel();
         categoriesModel = new CategoriesModel();
         observableListCategories = categoriesModel.getAllCategories();
@@ -178,15 +181,18 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    public void mouseClicked(MouseEvent e){
-        if (e.getClickCount() == 2 && !e.isConsumed()) {
-            e.consume();
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getClassLoader().getResource("gui/View/VideoPlayer.fxml"));
-
-        }
-
-    }
+    public void mouseClicked (ActionEvent event) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/View/VideoPlayer.fxml"));
+            Parent root = fxmlLoader.load();
+            VideoPlayerController videoPlayer = fxmlLoader.getController();
+            Stage stage = new Stage();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            Node n = (Node) event.getSource();
+            Stage stage1 = (Stage) n.getScene().getWindow();
+            }
 
     @FXML
     private void newMovieBtn (ActionEvent actionEvent) throws IOException, SQLServerException {
